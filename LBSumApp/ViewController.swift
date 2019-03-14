8ff31c84e4081b23038b76a07f9f68bdfbcd2dca
Wira Setiawan <wirasetiawan29@gmail.com>
2019-03-14 15:22:21 +0700

    init

---
+++ LBSumApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBSumApp
+//
+//  Created by Wira on 3/14/19.
+//  Copyright © 2019 Wira Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
