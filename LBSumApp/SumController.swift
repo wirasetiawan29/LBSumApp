2f1dcfb8159f2ed8839567aa53534fa950b93ed0
Wira Setiawan <wirasetiawan29@gmail.com>
2019-03-15 22:12:58 +0700

    viper on mode

--- LBSumApp/SumController.swift
+++ LBSumApp/SumController.swift
@@ -13,15 +13,60 @@ class SumController: NSObject {
     var viewController: ViewController?
     var router: SumRouter?
     var interactor: SumInteractor?
-    let a:Int = 0
-    let b:Int = 0
-    let n:Int = 0
+    var a:Int = 0
+    var n:Int = 0
+    var temp:Int = 0
+    var mode:Int = 0
+    var firstLoad:Bool = true
     
     init(viewController: ViewController?) {
         super.init()
         self.viewController = viewController
         interactor = SumInteractor(controller: self)
         router = SumRouter(withViewController: self.viewController)
+    }
+    
+    func resetValue(){
+        a = 0
+        n = 0
+        temp = 0
+        firstLoad = true
+        self.viewController?.totalLabel.text = "0"
+    }
+    
+    func configureInputValue(value:String) {
+        if value == "x" {
+            resetValue()
+        } else if firstLoad {
+            temp = Int(value)!
+            n = Int(value)!
+            firstLoad = false
+        } else {
+            a = Int(value)!
+            n = Int(value)!
+        }
+        
+        switch mode {
+        case 1:
+            viewController!.totalLabel.text =  "\(sum(a: a, b: temp))"
+            temp = sum(a: a, b: temp)
+        case 2:
+            viewController!.totalLabel.text =  "\(multiple(a: temp, b: a))"
+            let last = multiple(a: temp, b: a)
+            if last != 0 {
+                temp = last
+            }
+        case 3:
+            viewController!.totalLabel.text =  "\(prime(n: n))"
+        case 4:
+            if n > 1 {
+                viewController!.totalLabel.text =  "\(fibonacci(n: n))"
+            } else {
+                resetValue()
+            }
+        default:
+            viewController!.totalLabel.text =  "\(sum(a: a, b: temp))"
+        }
     }
 
     func sum(a:Int, b:Int) -> Int {
@@ -59,6 +104,87 @@ class SumController: NSObject {
             return array
         }
         
+    }
+    
+    //MARK: private function
+    func greenColor() -> UIColor {
+        return UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0)
+    }
+    
+    func plusButtonIsActive(isActive:Bool) {
+        if isActive {
+            viewController!.plusButton.backgroundColor = greenColor()
+            viewController!.plusButton.setTitleColor(UIColor.white, for: .normal)
+            multipleButtonIsActive(isActive: false)
+            primeButtonIsActive(isActive: false)
+            fibButtonIsActive(isActive: false)
+        } else {
+            viewController!.plusButton.backgroundColor = UIColor.white
+            viewController!.plusButton.setTitleColor(greenColor(), for: .normal)
+        }
+    }
+    
+    func multipleButtonIsActive(isActive:Bool) {
+        if isActive {
+            viewController!.multipleButton.backgroundColor = greenColor()
+            viewController!.multipleButton.setTitleColor(UIColor.white, for: .normal)
+            plusButtonIsActive(isActive: false)
+            primeButtonIsActive(isActive: false)
+            fibButtonIsActive(isActive: false)
+        } else {
+            viewController!.multipleButton.backgroundColor = UIColor.white
+            viewController!.multipleButton.setTitleColor(greenColor(), for: .normal)
+        }
+    }
+    
+    func primeButtonIsActive(isActive:Bool) {
+        if isActive {
+            viewController!.primeButton.backgroundColor = greenColor()
+            viewController!.primeButton.setTitleColor(UIColor.white, for: .normal)
+            plusButtonIsActive(isActive: false)
+            multipleButtonIsActive(isActive: false)
+            fibButtonIsActive(isActive: false)
+        } else {
+            viewController!.primeButton.backgroundColor = UIColor.white
+            viewController!.primeButton.setTitleColor(greenColor(), for: .normal)
+        }
+    }
+    
+    func fibButtonIsActive(isActive:Bool) {
+        if isActive {
+            viewController!.fibButton.backgroundColor = greenColor()
+            viewController!.fibButton.setTitleColor(UIColor.white, for: .normal)
+            plusButtonIsActive(isActive: false)
+            multipleButtonIsActive(isActive: false)
+            primeButtonIsActive(isActive: false)
+        } else {
+            viewController!.fibButton.backgroundColor = UIColor.white
+            viewController!.fibButton.setTitleColor(greenColor(), for: .normal)
+        }
+    }
+    
+    func onSelectMode(onMode:Int) {
+        mode = onMode
+        switch mode {
+        case 1:
+            temp = a
+            firstLoad = true
+            plusButtonIsActive(isActive: true)
+        case 2:
+            temp = a
+            firstLoad = true
+            multipleButtonIsActive(isActive: true)
+        case 3:
+            temp = a
+            firstLoad = true
+            primeButtonIsActive(isActive: true)
+        case 4:
+            temp = a
+            firstLoad = true
+            fibButtonIsActive(isActive: true)
+        default:
+            onSelectMode(onMode: 1)
+        }
     }
     
 }
--- LBSumApp/ViewController.swift
+++ LBSumApp/ViewController.swift
@@ -19,11 +19,6 @@ class ViewController: UIViewController {
     
     
     var controller: SumController?
-    var a:Int = 0
-    var n:Int = 0
-    var temp:Int = 0
-    var mode:Int = 0
-    var firstLoad:Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,124 +28,40 @@ class ViewController: UIViewController {
     
     func setUpData() {
         controller = SumController.init(viewController: self)
-        firstLoad = true
-        mode = 1
-        plusButton.backgroundColor = UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0)
-        plusButton.setTitleColor(UIColor.white, for: .normal)
+        controller?.firstLoad = true
+        controller!.onSelectMode(onMode: 1)
     }
     
     func setUpUI() {
         
     }
     
-    // MARK: private function
-    func resetValue(){
-        a = 0
-        n = 0
-        temp = 0
-        firstLoad = true
-        self.totalLabel.text = "0"
-    }
-    
     // MARK: action function
     @IBAction func onClickPlus(_ sender: Any) {
-        resetValue()
-        mode = 1
-        temp = a
-        firstLoad = true
-        plusButton.backgroundColor = UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0)
-        plusButton.setTitleColor(UIColor.white, for: .normal)
-        multipleButton.backgroundColor = UIColor.white
-        multipleButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        primeButton.backgroundColor = UIColor.white
-        primeButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        fibButton.backgroundColor = UIColor.white
-        fibButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
+        controller!.resetValue()
+        controller!.onSelectMode(onMode: 1)
     }
     
     @IBAction func onClickMultiple(_ sender: Any) {
-        resetValue()
-        mode = 2
-        temp = a
-        firstLoad = true
-        plusButton.backgroundColor = UIColor.white
-        plusButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        multipleButton.backgroundColor = UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0)
-        multipleButton.setTitleColor(UIColor.white, for: .normal)
-        primeButton.backgroundColor = UIColor.white
-        primeButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        fibButton.backgroundColor = UIColor.white
-        fibButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
+        controller!.resetValue()
+        controller!.onSelectMode(onMode: 2)
+        
     }
     
     @IBAction func onClickPrime(_ sender: Any) {
-        resetValue()
-        mode = 3
-        temp = a
-        firstLoad = true
-        plusButton.backgroundColor = UIColor.white
-        plusButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        multipleButton.backgroundColor = UIColor.white
-        multipleButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        primeButton.backgroundColor = UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0)
-        primeButton.setTitleColor(UIColor.white, for: .normal)
-        fibButton.backgroundColor = UIColor.white
-        fibButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
+        controller!.resetValue()
+        controller!.onSelectMode(onMode: 3)
+        
     }
     
     @IBAction func onClickFib(_ sender: Any) {
-        resetValue()
-        mode = 4
-        temp = a
-        firstLoad = true
-        plusButton.backgroundColor = UIColor.white
-        plusButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        multipleButton.backgroundColor = UIColor.white
-        multipleButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        primeButton.backgroundColor = UIColor.white
-        primeButton.setTitleColor(UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0), for: .normal)
-        fibButton.backgroundColor = UIColor.init(red: 132.0/255.0, green: 220.0/255.0, blue: 198.0/255.0, alpha: 1.0)
-        fibButton.setTitleColor(UIColor.white, for: .normal)
+        controller!.resetValue()
+        controller!.onSelectMode(onMode: 4)
+        
     }
     
     @IBAction func onClickNewCharacter(_ sender: Any) {
-        let button: UIButton! = (sender as! UIButton)
-        
-        let value = (button?.titleLabel?.text)!
-        
-        if value == "x" {
-           resetValue()
-        } else if firstLoad {
-            temp = Int(value)!
-            n = Int(value)!
-            firstLoad = false
-        } else {
-            a = Int(value)!
-            n = Int(value)!
-        }
-        
-        switch mode {
-        case 1:
-            self.totalLabel.text =  "\(controller!.sum(a: a, b: temp))"
-           temp = controller!.sum(a: a, b: temp)
-        case 2:
-            self.totalLabel.text =  "\(controller!.multiple(a: temp, b: a))"
-            let last = controller!.multiple(a: temp, b: a)
-            if last != 0 {
-                temp = last
-            }
-        case 3:
-            self.totalLabel.text =  "\(controller!.prime(n: n))"
-        case 4:
-            if n > 1 {
-                self.totalLabel.text =  "\(controller!.fibonacci(n: n))"
-            } else {
-                resetValue()
-            }
-        default:
-            self.totalLabel.text =  "\(controller!.sum(a: a, b: temp))"
-        }
-        
+        controller?.configureInputValue(value: ((sender as! UIButton).titleLabel?.text)!)
     }
 }
 
